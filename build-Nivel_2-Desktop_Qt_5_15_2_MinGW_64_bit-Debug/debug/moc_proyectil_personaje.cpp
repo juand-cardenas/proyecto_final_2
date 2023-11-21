4faca656b6b67a8810f149ee487630ed73b03e70
Gamb25 <143144941+Gamb25@users.noreply.github.com>
2023-11-21 16:26:51 -0500

    build-Nivel_2-Desktop_Qt_5_15_2_MinGW_64_bit-Debug\debug\enemigos.o build-Nivel_2-Desktop_Qt_5_15_2_MinGW_64_bit-Debug\debug\mainwindow.o build-Nivel_2-Desktop_Qt_5_15_2_MinGW_64_bit-Debug\debug\moc_enemigos.o build-Nivel_2-Desktop_Qt_5_15_2_MinGW_64_bit-Debug\debug\moc_mainwindow.cpp build-Nivel_2-Desktop_Qt_5_15_2_MinGW_64_bit-Debug\debug\moc_mainwindow.o build-Nivel_2-Desktop_Qt_5_15_2_MinGW_64_bit-Debug\debug\moc_proyectil_personaje.cpp build-Nivel_2-Desktop_Qt_5_15_2_MinGW_64_bit-Debug\debug\moc_proyectil_personaje.o build-Nivel_2-Desktop_Qt_5_15_2_MinGW_64_bit-Debug\debug\Nivel_2.exe build-Nivel_2-Desktop_Qt_5_15_2_MinGW_64_bit-Debug\debug\personaje.o build-Nivel_2-Desktop_Qt_5_15_2_MinGW_64_bit-Debug\debug\proyectil_personaje.o build-Nivel_2-Desktop_Qt_5_15_2_MinGW_64_bit-Debug\debug\qrc_sprites.cpp build-Nivel_2-Desktop_Qt_5_15_2_MinGW_64_bit-Debug\debug\qrc_sprites.o build-Nivel_2-Desktop_Qt_5_15_2_MinGW_64_bit-Debug\Makefile.Debug build-Nivel_2-Desktop_Qt_5_15_2_MinGW_64_bit-Debug\Makefile.Release
    git ignore

--- build-Nivel_2-Desktop_Qt_5_15_2_MinGW_64_bit-Debug/debug/moc_proyectil_personaje.cpp
+++ build-Nivel_2-Desktop_Qt_5_15_2_MinGW_64_bit-Debug/debug/moc_proyectil_personaje.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_Proyectil_personaje_t {
-    QByteArrayData data[1];
-    char stringdata0[20];
+    QByteArrayData data[6];
+    char stringdata0[56];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -32,10 +32,16 @@ struct qt_meta_stringdata_Proyectil_personaje_t {
     )
 static const qt_meta_stringdata_Proyectil_personaje_t qt_meta_stringdata_Proyectil_personaje = {
     {
-QT_MOC_LITERAL(0, 0, 19) // "Proyectil_personaje"
+QT_MOC_LITERAL(0, 0, 19), // "Proyectil_personaje"
+QT_MOC_LITERAL(1, 20, 6), // "Mov_up"
+QT_MOC_LITERAL(2, 27, 0), // ""
+QT_MOC_LITERAL(3, 28, 8), // "Mov_down"
+QT_MOC_LITERAL(4, 37, 8), // "Mov_left"
+QT_MOC_LITERAL(5, 46, 9) // "Mov_rigth"
 
     },
-    "Proyectil_personaje"
+    "Proyectil_personaje\0Mov_up\0\0Mov_down\0"
+    "Mov_left\0Mov_rigth"
 };
 #undef QT_MOC_LITERAL
 
@@ -45,21 +51,41 @@ static const uint qt_meta_data_Proyectil_personaje[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       4,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
 
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   34,    2, 0x08 /* Private */,
+       3,    0,   35,    2, 0x08 /* Private */,
+       4,    0,   36,    2, 0x08 /* Private */,
+       5,    0,   37,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
        0        // eod
 };
 
 void Proyectil_personaje::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    Q_UNUSED(_o);
-    Q_UNUSED(_id);
-    Q_UNUSED(_c);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Proyectil_personaje *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->Mov_up(); break;
+        case 1: _t->Mov_down(); break;
+        case 2: _t->Mov_left(); break;
+        case 3: _t->Mov_rigth(); break;
+        default: ;
+        }
+    }
     Q_UNUSED(_a);
 }
 
@@ -83,14 +109,25 @@ void *Proyectil_personaje::qt_metacast(const char *_clname)
     if (!_clname) return nullptr;
     if (!strcmp(_clname, qt_meta_stringdata_Proyectil_personaje.stringdata0))
         return static_cast<void*>(this);
-    if (!strcmp(_clname, "QGraphicsItem"))
-        return static_cast< QGraphicsItem*>(this);
+    if (!strcmp(_clname, "QGraphicsRectItem"))
+        return static_cast< QGraphicsRectItem*>(this);
     return QObject::qt_metacast(_clname);
 }
 
 int Proyectil_personaje::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 4)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 4;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 4)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 4;
+    }
     return _id;
 }
 QT_WARNING_POP
