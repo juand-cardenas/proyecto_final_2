8f951d4477b503d5c0a8deaf78963fbd6abce1b0
Gamb25 <143144941+Gamb25@users.noreply.github.com>
2023-12-03 20:12:28 -0500

    Se añadió nivel 2 a la carpeta
    Aun no se integra a nivel 1

---
+++ juego_fi/archivo h nivel_1/Nivel_2/mainwindow.h
@@ -0,0 +1,65 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+
+#include <QMainWindow>
+#include<QGraphicsScene>
+#include<QGraphicsPixmapItem>
+#include <QKeyEvent>
+#include <QMap>
+#include <QPixmap>
+#include <QMessageBox>
+#include <QDebug>
+#include <QVector>
+#include <QTime>
+#include"personaje.h"
+#include"enemigos.h"
+#include "proyectil_personaje.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    //METODOS
+    void keyPressEvent(QKeyEvent *e);
+    void mover_enemigo_colisionado(int key);
+    void mover_enemigo_colisionado_personaje(int key);
+    void Nivel_2_terminado(bool win_lose);
+    void Inicio_Nivel_2();
+
+
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QGraphicsScene *scene2;
+
+    QMap<int,Enemigos *> Mapa_enemigos;
+
+    QVector<Proyectil_personaje*> vector_proyectiles;
+    QVector<QGraphicsPixmapItem*> corazones;
+    QPixmap *cora=new QPixmap(":/Vida/sprites/Vida/heart pixel art 32x32.png");
+    QMessageBox *msgbox=new QMessageBox();
+
+    Personaje *p1,*p2;
+    QTime cuenta_regresiva;
+    QTimer *timer_cronometro, *timer_mov_colisiones_aparicion;
+private slots:
+    void mov_enemigos();
+    void colision_entre_enemigos();
+    void colision_enemigo_personaje();
+    void colision_enemigo_proyectil();
+    void aparicion_periodica_enemigos();
+    void fin_dejuego();
+
+
+
+
+};
+#endif // MAINWINDOW_H
---
+++ juego_fi/archivo h nivel_1/Nivel_2/proyectil_personaje.h
@@ -0,0 +1,57 @@
+#ifndef PROYECTIL_PERSONAJE_H
+#define PROYECTIL_PERSONAJE_H
+
+#include <QObject>
+#include <QGraphicsItem>
+#include <QGraphicsScene>
+#include <QPixmap>
+#include<Qpainter>
+#include<QGraphicsPixmapItem>
+#include<QTimer>
+#include<QList>
+#include<math.h>
+
+class Proyectil_personaje:public QObject,public QGraphicsItem
+{
+    Q_OBJECT
+    Q_INTERFACES(QGraphicsItem)
+
+private:
+
+    //ATRIBUTOS
+    double pos_x,pos_y,ancho=32,largo=32, velocidad_x=35,velocidad_y=28,tiempo=1,y_final;
+    char direccion_disparo;
+    QPixmap *mapa_proyec_per=new QPixmap(":/Proyectil/sprites/Proyectil/Bola_de_fuego.png");
+    QTimer *timer=new QTimer();
+
+
+public:
+    //METODOS
+    //CONSTRUCTOR
+    Proyectil_personaje();
+    Proyectil_personaje(int x,int y,char direccion);
+    //DECLARACIONES DE FUNCIONES QUE ERAN ABSTRACTAS
+    QRectF boundingRect()const;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = nullptr);
+    //MOVIMIENTO
+    void set_posiciones(double x,double y);
+
+
+    //DESTRUCTOR
+    ~Proyectil_personaje();
+    //MOVIMIENTO BALA
+    QTimer *getTimer() const;
+
+private slots:
+    void Mov_up();
+    void Mov_down();
+    void Mov_left();
+    void Mov_rigth();
+
+
+
+
+
+};
+
+#endif // PROYECTIL_PERSONAJE_H
